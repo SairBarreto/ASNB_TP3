3ea92b3d9ed7da81fa6c17f651314c73be50e482
Sair Barreto <sairgregori@gmail.com>
2022-11-13 16:53:52 -0300

    Agregado el menu

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+//#include "Animal.h"
+#include "Menu.h"
+
+using namespace std;
+
+int main()
+{
+    //Lista<Animal>* lista_animales = new Lista<Animal>;
+    int opcion = 0;
+
+    //leer_archivo(lista_animales);
+
+   do{
+        mostrar_menu();
+        opcion = menu_pedir_opcion();
+        menu_validar_opcion(opcion);
+        //procesar_opcion(opcion, lista_animales);
+    }while(opcion != SALIR);
+    
+    //delete lista_animales;
+
+    return 0;
+}
