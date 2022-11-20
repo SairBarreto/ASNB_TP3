e6cda4552e6d7f146c7bb7bce26b6ed32e600ccf
nicomp04 <nicolasmp99@hotmail.com>
2022-11-20 19:27:16 -0300

    floyd

--- Grafos.cpp
+++ Grafos.cpp
@@ -6,6 +6,18 @@ using namespace std;
 #define numV 8
 #define INF 999
 
+/*  INICIAL
+  0  1  2  3  4  5  6  7         
+  __ __ __ __ __ __ __ __
+0|0 |40|in|  |  |  |  |  |  Al siempre partir del (0,0) y solo moverse norte sur este u oeste
+1|1 |in|in|in|  |  |  |  |  esos seran los dos valores "fijos" y los demas infinito, el cual la funcion
+2|in|in|in|  |  |  |  |  |  camino_minimo se encargara de hallar el valor minimo
+3|in|in|  |0 |  |  |  |  |
+4|  |  |  |  |0 |  |  |  |
+5|  |  |  |  |  |0 |  |  | 
+6|  |  |  |  |  |  |0 |  |
+7|  |  |  |  |  |  |  |0 | 
+*/ 
 void camino_minimo(int matriz_mapa[numV][numV]){ //matriz_mapa ya cargada con costo de cada celda
     int i, j, k;
     int matriz_1[numV][numV];
